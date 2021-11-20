5b18f85d7861d43109b59626f12a6e4ae1b4d905
L200150110 <riski.fight@ymail.com>
2021-11-20 14:25:49 +0700

    bottom tab navigation done

---
+++ components/ButtonForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ButtonForm = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
+
+export default ButtonForm;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+// import bottom tab
+// import BottomTabs from "./../components/BottomTabs";
+
 const HomeScreen = () => {
   return (
     <View>
       <Text>Home Screen</Text>
+      {/* <BottomTabs /> */}
     </View>
   );
 };
