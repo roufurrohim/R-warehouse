ffa5c0c6e4fb2417ddb81a30afbf153935ae9359
L200150110 <riski.fight@ymail.com>
2021-11-20 17:46:54 +0700

    searchbar file terpisah

--- screens/CatatanScreen.js
+++ screens/CatatanScreen.js
@@ -3,25 +3,13 @@ import { View, Text, StyleSheet, TextInput } from "react-native";
 import Icon, { Icons } from "./../components/Icons";
 import { windowHeight, windowWidth } from "./../components/Dimentions";
 import Color from "./../components/Colors";
+import SearchBar from "./../components/SearchBar";
 
 const CatatanScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.searchBarContainer}>
-        <View style={styles.searchBar}>
-          <Icon
-            type={Icons.Feather}
-            name={"search"}
-            color={Color.primary}
-            style={styles.searchIcon}
-          />
-          <TextInput
-            style={styles.searchBarInput}
-            onChange={() => {}}
-            placeholder="Masukkan nama barang yang dicari"
-          />
-        </View>
-      </View>
+      <SearchBar placeholder="Masukkan nama barang yang dicari" />
+      <View style={styles.listContainer} />
     </View>
   );
 };
@@ -32,26 +20,5 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
   },
-  searchBarContainer: {
-    height: 80,
-    width: "100%",
-    backgroundColor: Color.primary,
-    justifyContent: "center",
-    padding: 10,
-    borderBottomLeftRadius: 15,
-    borderBottomRightRadius: 15,
-  },
-  searchBar: {
-    flexDirection: "row",
-    backgroundColor: Color.white,
-    borderRadius: 15,
-    paddingHorizontal: 20,
-    alignItems: "center",
-  },
-  searchIcon: {
-    marginRight: 10,
-  },
-  searchBarInput: {
-    fontSize: 16,
-  },
+  listContainer: {},
 });
