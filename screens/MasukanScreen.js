79ef55158a8f31addae0407eedeaa06b93a5ffb3
roufurrohim <muhamadbarruroufurrohim@gmail.com>
2021-11-21 11:43:23 +0700

    perbaikan input data

--- screens/MasukanScreen.js
+++ screens/MasukanScreen.js
@@ -1,40 +1,46 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TextInput,
   TouchableOpacity,
-} from "react-native";
-import { windowWidth, windowHeight } from "./../components/Dimentions";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import Colors from "./../components/Colors";
-import InputForm from "./../components/InputForm";
+  KeyboardAvoidingView,
+} from 'react-native';
+import {windowWidth, windowHeight} from './../components/Dimentions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Colors from './../components/Colors';
+import InputForm from './../components/InputForm';
 
 const MasukanScreen = () => {
-  // const [form, setForm] = useState({
-  //   nameItem: "",
-  // });
+  const keyboardVerticalOffset = Platform.OS === 'ios' ? 40 : 0;
+  // {"harga": 5000, "jumlah": 14, "key": "1", "nama": "Dunkin' Donuts", "value": "Dunkin' Donuts", tipe: "makanan"}
+  const [form, setForm] = useState({
+    value: '',
+    tipe: '',
+    jumlah: '',
+    harga: '',
+  });
 
   var data = [];
-
+  console.log(form);
   const tulisData = async () => {
     try {
       await AsyncStorage.setItem(
-        "Database Catatan Barang",
-        JSON.stringify(data)
+        'Database Catatan Barang',
+        JSON.stringify(data),
       );
     } catch (error) {
       // Error saving data
       console.log(error);
     }
-    console.log("tulis data selesai");
+    console.log('tulis data selesai');
   };
 
   const readData = async () => {
     let data;
     try {
-      data = await AsyncStorage.getItem("Database Catatan Barang");
+      data = await AsyncStorage.getItem('Database Catatan Barang');
     } catch (error) {
       // Error saving data
       console.log(error);
@@ -44,55 +50,77 @@ const MasukanScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Input Data</Text>
-      <InputForm label="ID Barang:" value="1" editable={false} />
-      <InputForm label="Nama Barang:" placeholder="Masukkan Nama Barang" />
-      <InputForm label="Tipe Barang:" placeholder="Masukkan Tipe Barang" />
-      <InputForm label="Jumlah Barang:" placeholder="Masukkan Jumlah Barang" />
-      <InputForm label="Harga Barang:" placeholder="Masukkan Harga Barang" />
-      <TouchableOpacity
-        style={styles.fixToText}
-        onPress={() => {
-          tulisData();
-        }}
-      >
-        <Text
-          style={styles.btnSave}
-          // title="Save"
-        >
-          Save
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.btnCancel}
-        onPress={() => {
-          readData();
-        }}
-      >
-        <Text style={styles.textCancel}>Cancel</Text>
-      </TouchableOpacity>
+      <KeyboardAvoidingView
+        behavior="position"
+        keyboardVerticalOffset={keyboardVerticalOffset}>
+        <Text style={styles.text}>Input Data</Text>
+
+        <InputForm label="ID Barang:" value="1" editable={false} />
+        <InputForm
+          label="Nama Barang:"
+          placeholder="Masukkan Nama Barang"
+          onChangeText={value => setForm({...form, value: value})}
+        />
+        <InputForm
+          label="Tipe Barang:"
+          placeholder="Masukkan Tipe Barang"
+          onChangeText={value => setForm({...form, tipe: value})}
+        />
+        <InputForm
+          label="Jumlah Barang:"
+          placeholder="Masukkan Jumlah Barang Per Karton"
+          onChangeText={value => setForm({...form, jumlah: value})}
+          keyboardType="numeric"
+        />
+        <InputForm
+          label="Harga Barang:"
+          placeholder="Masukkan Harga Barang"
+          onChangeText={value => setForm({...form, harga: value})}
+          keyboardType="numeric"
+        />
+        <TouchableOpacity
+          style={styles.fixToText}
+          onPress={() => {
+            tulisData();
+          }}>
+          <Text
+            style={styles.btnSave}
+            // title="Save"
+          >
+            Save
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnCancel}
+          onPress={() => {
+            readData();
+          }}>
+          <Text style={styles.textCancel}>Cancel</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
     paddingTop: 30,
   },
   text: {
     fontSize: 30,
-    color: "black",
-    fontWeight: "bold",
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   fixToText: {
     marginTop: 50,
     width: 300,
     height: 45,
     backgroundColor: Colors.primary,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 15,
   },
   btnCancel: {
@@ -101,31 +129,31 @@ const styles = StyleSheet.create({
     height: 45,
     borderWidth: 1,
     borderColor: Colors.primary,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 15,
   },
   btnSave: {
-    display: "flex",
-    color: "white",
+    display: 'flex',
+    color: 'white',
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   textCancel: {
-    display: "flex",
+    display: 'flex',
     color: Colors.primary,
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   saveButton: {
     width: windowWidth - 50,
     height: 50,
     backgroundColor: Colors.primary,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   saveButtonText: {
-    color: "white",
+    color: 'white',
   },
 });
 
